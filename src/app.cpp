73181e33eece0189240b20dc9f7adcb4242822a4
NouTimbaler <f.toda.c@gmail.com>
2021-03-13 13:12:04 +0100

    refinant cosetes

--- src/app.cpp
+++ src/app.cpp
@@ -64,7 +64,7 @@ int main(void)
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
-		ImGui_ImplOpenGL3_Init("#version 130");
+		ImGui_ImplOpenGL3_Init("#version 330 core");
 		ImGui::StyleColorsDark();
 
 		std::cout << glGetString(GL_VERSION) << std::endl;
@@ -72,10 +72,10 @@ int main(void)
 
 		// vertex definition
 		float positions[] = {
-				100.0f, 100.0f, 0.0f, 0.0f,
-				200.0f, 100.0f, 1.0f, 0.0f,
-				200.0f, 200.0f, 1.0f, 1.0f,
-				100.0f, 200.0f, 0.0f, 1.0f
+				0.0f, 0.0f, 0.0f, 0.0f,
+				100.0f, 0.0f, 1.0f, 0.0f,
+				100.0f, 100.0f, 1.0f, 1.0f,
+				0.0f, 100.0f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -102,7 +102,7 @@ int main(void)
 		ib.UnBind();
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
 		glm::mat4 mvp = proj * view * model;
@@ -119,12 +119,9 @@ int main(void)
 		shader.UnBind();
 
 
-		glm::mat4 model2 = glm::translate(glm::mat4(1.0f), glm::vec3(600, 200, 0));
-		glm::mat4 mvp2 = proj * view * model2;
 		Shader shader2("res/shaders/basic2.shader");
 		shader2.Bind();
 		shader2.SetUniform4f("u_Color", 0.8f, 0.1f, 0.8f, 1.0f);
-		shader2.SetUniformMat4f("u_MVP", mvp2);
 
 
 		Renderer renderer;
@@ -132,7 +129,8 @@ int main(void)
 		float r = 0.0f;
 		float increment = 0.05f;
 
-		float f;
+		glm::vec3 translation(600, 200, 0);
+		float f = 0.0f;
 		char buf[200];
 
 		/* Loop until the user closes the window */
@@ -142,39 +140,41 @@ int main(void)
 				renderer.Clear();
 
 
-
-
-
 				shader.Bind();
 				texture.Bind();
 				renderer.Draw(va, ib, shader);
 				shader.UnBind();
 				texture.UnBind();
 
-
+		glm::mat4 model2 = glm::translate(glm::mat4(1.0f), translation);
+		glm::mat4 mvp2 = proj * view * model2;
 				shader2.Bind();
-				shader2.SetUniform4f("u_Color", r, 0.1f, 0.8f, 1.0f);
+				shader2.SetUniform4f("u_Color", f, 0.1f, 0.8f, 1.0f);
+		shader2.SetUniformMat4f("u_MVP", mvp2);
 				renderer.Draw(va, ib, shader2);
 				shader2.UnBind();
 
-
 				r += increment;
 				if (r > 1.0f or r < 0.0f) increment *= -1;
+
+
 
 				ImGui_ImplOpenGL3_NewFrame();
 				ImGui_ImplGlfw_NewFrame();
 				ImGui::NewFrame();
 
-				ImGui::Begin("hello");
-				ImGui::Text("Hello world %d", 123);
-				if(ImGui::Button("Save"))
-						std::cout << "saved! xdxd" << std::endl;
-				ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+				ImGui::Begin("Tester");
+				ImGui::Text("This is the tester window of ImGui");
+				ImGui::SliderFloat("Color", &f, 0.0f, 1.0f);
+				ImGui::SliderFloat3("Position", &translation.x, 0.0f, 960.0f);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 				ImGui::End();
 
 				ImGui::Render();
 				ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
 
 				/* Swap front and back buffers */
 				glfwSwapBuffers(window);
